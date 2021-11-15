eaaf223a7ff45ffa7a12350c9011a6ae9ee12149
Ferdinand Vanderborght <ferdivdb@Ferdinands-MacBook-Air.local>
2021-11-15 18:16:32 +0100

    test

--- README.txt
+++
@@ -1,7 +0,0 @@
-Welcome to my website Project,
-
-Ill try to create my landing page with Django !
-
-But first lest's play with html css !
-
-i'll start soon..
--- landing_page/apps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.apps_index, name='apps_index'),
-    path('app/<str:name>/', views.app, name='app'),
-]
